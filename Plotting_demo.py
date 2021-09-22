5e3f0c5cf487b0f962e68c748eb9c163891a1cdb
Sarat Chandra <72928593+carbform@users.noreply.github.com>
2021-09-22 15:12:47 +0530

    Update Plotting_demo.py

--- Plotting_demo.py
+++ Plotting_demo.py
@@ -16,26 +16,16 @@ import geopandas as gpd
 from shapely.geometry import mapping
 import cartopy.crs as ccrs
 from cartopy.io.shapereader import Reader
-import imdlib as imd
-import statsmodels.api as sm
-import matplotlib.dates as mdates
-import cftime
-import scipy.fft as fft
-import seaborn as sns
 import matplotlib.pyplot as plt
 #%% plot properties #### fuck out and find out ######
 plot.rc.reset()
-# plot.register_fonts('/home/sarat/anaconda3/pkgs/proplot-0.8.1-pyhd8ed1ab_0/site-packages/proplot/fonts/Simplex.ttf')
-# plot.register_fonts('/home/sarat/anaconda3/pkgs/proplot-0.8.1-pyhd8ed1ab_0/site-packages/proplot/fonts/ibm.ttf')
-# plot.register_fonts('/home/sarat/anaconda3/pkgs/proplot-0.8.1-pyhd8ed1ab_0/site-packages/proplot/fonts/simpl_bold.ttf')
 plot.register_fonts('/home/sarat/anaconda3/pkgs/proplot-0.8.1-pyhd8ed1ab_0/site-packages/proplot/fonts/IBMPlexSans-SemiBold.ttf')
 plot.rc['font.name'] = 'IBM Plex Sans'
 plot.rc['font.weight']='bold'
 plot.rc['font.size']=10
 plot.rc['tick.labelsize']=10
-
-# plot.rc['xtick.minor.visible']     =   False
-# plot.rc['ytick.minor.visible']     =   False
+plot.rc['xtick.minor.visible']     =   False
+plot.rc['ytick.minor.visible']     =   False
 plot.rc['tick.len']=2
 plot.rc['meta.width']=1.5
 plot.rc['subplots.tight']= True
@@ -43,8 +33,7 @@ plot.rc['grid']=True
 plot.rc['grid.linewidth']=0.25
 plot.rc['grid.linestyle']=(0, (5, 10))
 plot.rc['tick.dir']= 'out'
-
-# plot.rc['colorbar.insetpad']='0.5em'
+plot.rc['colorbar.insetpad']='0.5em'
 plot.rc['xtick.major.size']=3
 plot.rc['ytick.major.size']=3
 
@@ -68,9 +57,9 @@ fname2='/media/sarat/Study/Bundelkhand/shape/bundelkhand.shp'
 #### Figure starts ######
 fig, axs=plot.subplots(ncols=2,nrows=1, proj='cyl', dpi=300,
                        tight=True) ### nrows and ncols for subplots ###
-##### proj = 'cyl' Dont change ###
+##### proj = 'cyl' Dont change unless you are using a different map projection. ###
 #### dpi = 300 ( recommended ) , 600 , 1200 
-#### lata and lon limits #####
+#### latitude and longitude limits #####
 lat_min = 6 # Change accordingly now for India
 lat_max = 38 # lat max
 lon_min = 66 ###
@@ -93,7 +82,7 @@ axs.format(
 ####abcloc='ll', abc location
 #### gridminor=False; if true it will show all gridline of lat , lon
     
-    #####################################################
+#####################################################
    ######## Subplots ############# ### contourf for contours
    #### pcolormesh for pixel coloes ####
 ######################### Each subplot axis is numbered as axs[0] or axs[1] etc....]
